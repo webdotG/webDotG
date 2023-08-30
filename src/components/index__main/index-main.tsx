31f46d3598c5fdb3cec90c778798ee9db2929cf7
Grant Kirill <webdotg.inbox@gmail.com>
2023-08-30 21:31:31 +0300

    base style index main

---
+++ src/components/index__main/index-main.tsx
@@ -0,0 +1,12 @@
+import MainTitle from "./main-title/main-title";
+
+function IndexMain() {
+
+  return (
+    <main>
+      <MainTitle />
+    </main>
+  )
+}
+
+export default IndexMain;
---
+++ src/components/index__main/main-title/main-title.tsx
@@ -0,0 +1,18 @@
+import './main-title.css'
+
+function MainTitle() {
+
+  return (
+    <section className='main-title__wrapper'>
+      <h1 className='main-title Roboto-Thin'>webDotG</h1>
+      <div className='main-title__text-wrapper'>
+        <p className='main-title__text Roboto-Regular'>сайты</p>
+        <p className='main-title__text Roboto-Regular'>приложения</p>
+        <p className='main-title__text Roboto-Regular'>чат боты</p>
+        <p className='main-title__text Roboto-Regular'>email письма</p>
+      </div>
+    </section>
+  )
+}
+
+export default MainTitle;

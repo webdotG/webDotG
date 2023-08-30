37ce02d771ce5dd55d98799aabee9a31c138a980
Grant Kirill <webdotg.inbox@gmail.com>
2023-08-30 21:51:05 +0300

    base style index main

--- src/components/index__main/main-title/main-title.tsx
+++ src/components/index__main/main-title/main-title.tsx
@@ -5,6 +5,7 @@ function MainTitle() {
   return (
     <section className='main-title__wrapper'>
       <h1 className='main-title Roboto-Thin'>webDotG</h1>
+      {/* <h6 className='main-title__title--seconde Roboto-Regular'>создаю и улучшаю :</h6> */}
       <div className='main-title__text-wrapper'>
         <p className='main-title__text Roboto-Regular'>сайты</p>
         <p className='main-title__text Roboto-Regular'>приложения</p>

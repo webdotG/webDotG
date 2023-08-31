36b32442f2071907ca168ef749089e280c9c0af6
Grant Kirill <webdotg.inbox@gmail.com>
2023-08-31 17:53:41 +0300

    main price add test

--- src/components/index__main/main-portfolio/main-portfolio.tsx
+++ src/components/index__main/main-portfolio/main-portfolio.tsx
@@ -4,8 +4,13 @@ import './main-portfolio.css'
 function MainPortfolio() {
 
   return (
-    <div className="main-portfolio__wrapper color">
-      <img className="portfoli__lanka-booking" src="../../../../public/img/portfoli__lanka-booking.png" />
+    <div className="main-portfolio__wrapper ">
+      <div className='container'>
+        <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+        <div className="middle">
+          <div className="text">John Doe</div>
+        </div>
+      </div>
     </div>
   )
 }

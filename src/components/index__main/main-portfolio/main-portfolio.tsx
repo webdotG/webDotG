9e476b381ad74684844ec4f1c80b5f4f962e01ab
Grant Kirill <webdotg.inbox@gmail.com>
2023-09-03 17:18:35 +0300

    add main portfolio gallery

--- src/components/index__main/main-portfolio/main-portfolio.tsx
+++ src/components/index__main/main-portfolio/main-portfolio.tsx
@@ -5,10 +5,35 @@ function MainPortfolio() {
 
   return (
     <div className="main-portfolio__wrapper ">
-      <div className='container'>
-        <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
-        <div className="middle">
-          <div className="text">John Doe</div>
+      <div className='main-portfolio'>
+        <div className='container gallery__item gallery__item--1'>
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <div className="middle">
+            <div className="text">John Doe</div>
+          </div>
+          di</div>
+        <div className='container gallery__item gallery__item--2'>
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <div className="middle">
+            <div className="text">John Doe</div>
+          </div>
+        </div><div className='container gallery__item gallery__item--3'>
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <div className="middle">
+            <div className="text">John Doe</div>
+          </div>
+        </div>
+        <div className='container gallery__item gallery__item--4'>
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <div className="middle">
+            <div className="text">John Doe</div>
+          </div>
+        </div>
+        <div className='container gallery__item gallery__item--5'>
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <div className="middle">
+            <div className="text">John Doe</div>
+          </div>
         </div>
       </div>
     </div>

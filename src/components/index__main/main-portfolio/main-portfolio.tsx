18a6a5ee0815591627f96297666b2154a771213d
Grant Kirill <webdotg.inbox@gmail.com>
2023-09-04 13:10:16 +0300

    dkmgf

--- src/components/index__main/main-portfolio/main-portfolio.tsx
+++ src/components/index__main/main-portfolio/main-portfolio.tsx
@@ -7,30 +7,30 @@ function MainPortfolio() {
     <div className="main-portfolio__wrapper ">
       <div className='main-portfolio'>
         <div className='container gallery__item gallery__item--1'>
-          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfolio--5.jpg" />
           <div className="middle">
             <div className="text">John Doe</div>
           </div>
           di</div>
         <div className='container gallery__item gallery__item--2'>
-          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfolio--1.png" />
           <div className="middle">
             <div className="text">John Doe</div>
           </div>
         </div><div className='container gallery__item gallery__item--3'>
-          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfolio--2.png" />
           <div className="middle">
             <div className="text">John Doe</div>
           </div>
         </div>
         <div className='container gallery__item gallery__item--4'>
-          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfolio--3.png" />
           <div className="middle">
             <div className="text">John Doe</div>
           </div>
         </div>
         <div className='container gallery__item gallery__item--5'>
-          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfoli__lanka-booking.png" />
+          <img className="portfoli__lanka-booking color" src="../../../../public/img/portfolio--4.jpg" />
           <div className="middle">
             <div className="text">John Doe</div>
           </div>

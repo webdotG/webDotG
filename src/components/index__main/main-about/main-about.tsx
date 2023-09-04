5fcefd111ce946e6b54ec7a1c85ec6c577c79860
Grant Kirill <webdotg.inbox@gmail.com>
2023-09-04 17:39:28 +0300

    dfd

--- src/components/index__main/main-about/main-about.tsx
+++ src/components/index__main/main-about/main-about.tsx
@@ -4,22 +4,27 @@ function MainAbout() {
 
   return (
     <div className='main-about__wrapper'>
-      <h2 className='main-about__title'>как я работаю</h2>
-      <ul className='main-abou__list'>
-        <li className='main-abou__item'>изучаю задачи, составляю ТЗ
-          <img src='../../../../public/img/icon--thinking.png'/>
+      <h2 className='main-about__title'>Процесс работы </h2>
+      <ul className='main-about__list'>
+        <li className='main-abou__item'>
+          <img src='../../../../public/img/icon--thinking.png' />
+          изучаю задачи, составляю ТЗ
         </li>
-        <li className='main-abou__item'>разрабатываю концепцию и дизайн
-        <img src='../../../../public/img/icon--idea.png' />
+        <li className='main-abou__item'>
+          <img src='../../../../public/img/icon--idea.png' />
+          разрабатываю концепцию и дизайн
         </li>
-        <li className='main-abou__item'>верстаю и программирую
-        <img src='../../../../public/img/icon--data.png' />
+        <li className='main-abou__item'>
+          <img src='../../../../public/img/icon--data.png' />
+          верстаю и программирую
         </li>
-        <li className='main-abou__item'>тестирую
-        <img src='../../../../public/img/icon--test.png'/>
+        <li className='main-abou__item'>
+          <img src='../../../../public/img/icon--test.png' />
+          тестирую
         </li>
-        <li className='main-abou__item'>запускую
-        <img src='../../../../public/img/icon--start.png'/>
+        <li className='main-abou__item'>
+          <img src='../../../../public/img/icon--start.png' />
+          запускую
         </li>
       </ul>
     </div>
